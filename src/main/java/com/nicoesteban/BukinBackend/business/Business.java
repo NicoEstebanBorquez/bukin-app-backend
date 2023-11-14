da7db96c1597b6d0759cd078460a12dec79a16ed
NicoEstebanBorquez <43449804+NicoEstebanBorquez@users.noreply.github.com>
2023-11-14 18:47:29 +0100

    - Actualización a SpringBoot 3.1.0

--- src/main/java/com/nicoesteban/BukinBackend/business/Business.java
+++ src/main/java/com/nicoesteban/BukinBackend/business/Business.java
@@ -1,11 +1,11 @@
 package com.nicoesteban.BukinBackend.business;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Entity
